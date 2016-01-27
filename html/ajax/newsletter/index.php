1a544698240846739ff103b9cbb11d9b4a79ba34
Stev <topshelfsmith@gmail.com>
2016-01-27 09:42:24 -0500

    added newsletter subscription

--- html/ajax/newsletter/index.php
+++ html/ajax/newsletter/index.php
@@ -18,8 +18,9 @@ $db2use = array(
 
 /* LOAD FUNC-CLASS-LIB */
 require_once('libraries/drill/drill.php');
-$drill = new \Gajus\Drill\Client($apikey['mandrill']);
+require_once('classes/mailchimp.class.php');
 class BannerException extends Exception{}
+
 
 
 
@@ -35,71 +36,96 @@ $data1 = (isset($_POST['d1'])) ? $_POST['d1'] : null;
 
 try{
 
+    $chimp = new \DrewM\MailChimp\MailChimp($apikey['mailchimp']['connect']);
+
     switch($step){
 
         case '1': {
             $error  = '0';
             $h1    = 'Newsletter';
             $html  = '
-
+                <p>Subscribe to our newsletter to stay informed about Connect social events, and Playgroup dates and times.</p>
+                <label>Name</label>
+                <input type="text" id="newsletter-name">
+                <label>Email</label>
+                <input type="text" id="newsletter-email">
+                <button id="newsletter-submit">Subscribe</button>
             ';
             break;
         }
 
         case '2': {
 
+            if(filter_var($data1['email'], FILTER_VALIDATE_EMAIL)){
+                $email = $data1['email'];
+            }else{
+                throw new BannerException('<li>You did not enter a valid email address.</li>');
+            }
 
+            $name = preg_replace('/[^0-9a-zA-Z\s]/', '', $data1['name']);
 
+            $parts = explode(" ", $name);
+            $lastname = array_pop($parts);
+            $firstname = implode(" ", $parts);
+
+            $args = array(
+                'email_address'	=> $email,
+                'status'		=> 'subscribed',
+                'merge_fields'	=> array(
+                    'FNAME'		=> $firstname,
+                    'LNAME'		=> $lastname
+                )
+            );
+            $r = $chimp->post('lists/'.$apikey['mailchimp']['connect_list'].'/members', $args);
+
+            switch ($r['status']){
+                case 'subscribed' : {
+                    $error  = '0';
+                    $h1     = 'Newsletter';
+                    $html   = '
+                        <p>You have been successfully subscribed to the HOA Connect newsletter.</p>
+                        <table>
+                            <tr>
+                                <th>First Name</th>
+                                <td>'.$firstname.'</td>
+                            </tr>
+                            <tr>
+                                <th>Last Name</th>
+                                <td>'.$lastname.'</td>
+                            </tr>
+                            <tr>
+                                <th>Email</th>
+                                <td>'.$email.'</td>
+                            </tr>
+                        </table>
+                    ';
+                    break;
+                }
+                case 400 : {
+                    if($r['title'] = 'Member Exists'){
+                        $error  = '0';
+                        $h1     = 'Newsletter';
+                        $html   = '<p>Looks like you might already be subscribed. To be sure, <a href="http://eepurl.com/ysPFz">adust your email preferences here</a>.</p>';
+                    }else{
+                        $error  = '3';
+                        $html  .= '<li>'.$r['status'].'</li>';
+                        $html  .= '<li>'.$r['title'].'</li>';
+                    }
+                    break;
+                }
+                default : {
+                    throw new Exception('There was error with the newsletter form. (ref: mailchimp - '.$r['status'].' - '.$r['title'].')');
+                    break;
+                }
+            }
+
+            break;
         }
 
         default:
             throw new Exception('There was error with the newsletter form. (ref: invalid step)');
     }
-} catch (\Gajus\Drill\Exception\RuntimeException\ValidationErrorException $e) {
-    // @see https://mandrillapp.com/api/docs/messages.html
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\RuntimeException\UserErrorException $e) {
-    // @see https://mandrillapp.com/api/docs/messages.html
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\RuntimeException\UnknownSubaccountException $e) {
-    // @see https://mandrillapp.com/api/docs/messages.html
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\RuntimeException\PaymentRequiredException $e) {
-    // @see https://mandrillapp.com/api/docs/messages.html
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\RuntimeException\GeneralErrorException $e) {
-    // @see https://mandrillapp.com/api/docs/messages.html
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\RuntimeException\ValidationErrorException $e) {
-    // @see https://mandrillapp.com/api/docs/messages.html
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\RuntimeException $e) {
-    // All possible API errors.
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\InvalidArgumentException $e) {
-    // Invalid SDK use errors.
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
-} catch (\Gajus\Drill\Exception\DrillException $e) {
-    // Everything.
-    $error  = '1';
-    $h1     = 'Newsletter - Error';
-    $html   = '<p>There was error with the newsletter form. (ref: mandrill)</p>';
+
 }catch(mysqli_sql_exception $e){
     $error  = '1';
     $h1     = 'Newsletter - Error';
--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,3 @@
-
-
 function connectCleanup(){
 	closeTopError();
 	document.getElementById('modal_h1').innerHTML = '';
@@ -7,8 +5,6 @@ function connectCleanup(){
 	$('#ajax-modal').removeClass('contact-form');
 	$('#modal_close').unbind('click');
 }
-
-
 
 function contact(step, data1){
 	fullScreenLoad('show');
@@ -48,6 +44,60 @@ function contact(step, data1){
 	ajx.fail( function(){
 		document.getElementById('modal_h1').innerHTML = 'Error';
 		document.getElementById('modal_content').innerHTML = '<p>There was an error with the contact form. Please try again.</p><p>(ref: ajax fail)</p>';
+		showModal('ajax-modal');
+		fullScreenLoad('hide');
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function newsletter(step, data1){
+	fullScreenLoad('show');
+	$('#modal_close').click(function(){ connectCleanup(); });
+    $('#ajax-modal').addClass('contact-form');
+	var ajx = $.post(
+		"/ajax/newsletter/",
+		{s:step, d1:data1},
+		function( data ) {
+			if(data.error === '0' || data.error === '1'){
+				document.getElementById('modal_h1').innerHTML = data.h1;
+				document.getElementById('modal_content').innerHTML = data.html;
+				if(step === 1){
+					showModal('ajax-modal');
+					$('#newsletter-submit').click(function() {
+                        var dd = {
+                            "name" : document.getElementById('newsletter-name').value,
+							"email" : document.getElementById('newsletter-email').value
+                        };
+						closeTopError();
+                        newsletter(2, dd);
+					});
+				}
+				fullScreenLoad('hide');
+            }
+			if(data.error === '3'){
+				topError(data.html);
+				fullScreenLoad('hide');
+			}
+		},
+		"json"
+	);
+	ajx.fail( function(){
+		document.getElementById('modal_h1').innerHTML = 'Error';
+		document.getElementById('modal_content').innerHTML = '<p>There was an error with the newsletter form. Please try again.</p><p>(ref: ajax fail)</p>';
 		showModal('ajax-modal');
 		fullScreenLoad('hide');
 	});
